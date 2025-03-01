e24495c49e3ad69ec331e370054bf79c6aa637f1
GL3MON <harisshragavarchives@gmail.com>
2025-03-02 00:45:06 +0530

    Updated Env Access

--- main_api.py
+++ main_api.py
@@ -2,6 +2,10 @@ from apoorvbackend.src.logger import logger
 import importlib
 from fastapi import FastAPI
 import uvicorn
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = FastAPI()
 
@@ -13,11 +17,6 @@ async def root():
 @app.get("/{level}/{actor}")
 def chat_with_actor(level: str, actor: str):
     logger.info(f"Chatting with actor {actor} at level {level}")
-    try:
-        actor_worflow = importlib.import_module(f"apoorvbackend.src.agents.{level}.{actor}.workflow")
-        actor_class = getattr(actor_worflow, f"{actor.capitalize()}Agent")
-        actor = actor_class().complie()
-    
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1")
