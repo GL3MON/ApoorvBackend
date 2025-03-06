4d8bedfece58fa445fa64b7f872a630a8aa3f117
jg-eno <jglenenosh@gmail.com>
2025-03-06 11:41:32 +0530

    Added API Keys and LoadBalancer Integration

--- apoorvbackend/src/llm_handler/llm.py
+++ apoorvbackend/src/llm_handler/llm.py
@@ -1,6 +1,7 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import os
+from load_balancer import LoadBalancer
 
 load_dotenv()
 
@@ -9,11 +10,17 @@ class LLM:
 
     @staticmethod
     def get_llm(temperature = 0.5, timeout=10, max_retries=4):
+        lb = LoadBalancer(n=10,ct=10)
+        key = lb.StdDev()
         llm = ChatGoogleGenerativeAI(
             model=os.getenv("MODEL_NAME"),
+            api_key=key,
             temperature=temperature,           
             timeout=timeout,
             max_retries=max_retries,
         )
         
         return llm
+
+l = LLM()
+print(l.get_llm())
